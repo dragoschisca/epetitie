caa585a95e8331c64567e490884d250ebb393c79
Dragos Chisca <dragos-nicolae.chisca@esempla.com>
2026-09-07 16:56:45 +0300

    feat: changing ai model

--- backend/src/main/java/md/gov/epetitie/EpetitieApplication.java
+++ backend/src/main/java/md/gov/epetitie/EpetitieApplication.java
@@ -2,8 +2,10 @@ package md.gov.epetitie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class EpetitieApplication {
 
     public static void main(String[] args) {
